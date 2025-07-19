1f963ae129130a096c01fac19d67067a0b1d7bf8
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-07-20 01:25:17 +0330

    add loging into project

--- core/envs/production.py
+++ core/envs/production.py
@@ -1,3 +1,6 @@
+import datetime
+import os
+
 from core.settings import *
 
 ALLOWED_HOSTS = config("PRODUCTION_ALLOWED_HOSTS", cast=Csv())
@@ -65,3 +68,44 @@ AWS_ACCESS_KEY_ID = config('ARVAN_AWS_ACCESS_KEY_ID', cast=str)
 AWS_SECRET_ACCESS_KEY = config('ARVAN_AWS_SECRET_ACCESS_KEY', cast=str)
 AWS_STORAGE_BUCKET_NAME = config('ARVAN_AWS_STORAGE_BUCKET_NAME', cast=str)
 AWS_S3_ENDPOINT_URL = config('ARVAN_AWS_S3_ENDPOINT_URL', cast=str)
+
+# with logging django
+log_dir = os.path.join(BASE_DIR / 'general_log_django', datetime.date.today().strftime("%Y-%m-%d"))
+os.makedirs(log_dir, exist_ok=True)
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "color": {
+            "()": "colorlog.ColoredFormatter",
+            "format": "%(log_color)s%(levelname)s %(reset)s%(asctime)s %(module)s %(process)d %(thread)d %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "error_file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "formatter": "color",
+            "filename": os.path.join(BASE_DIR / log_dir / 'error_file.log')
+        },
+        "warning_file": {
+            "level": "WARN",
+            "class": "logging.FileHandler",
+            "formatter": "color",
+            "filename": os.path.join(BASE_DIR / log_dir / 'warning_file.log')
+        },
+        "critical_file": {
+            "level": "CRITICAL",
+            "class": "logging.FileHandler",
+            "formatter": "color",
+            "filename": os.path.join(BASE_DIR / log_dir / 'critical_file.log')
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["warning_file", "critical_file", "error_file"],
+            'propagate': True,
+        }
+    }
+}
