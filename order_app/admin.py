f4b9baae5dc3618a0ad464b6c2131fa37ad7581d
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-07-25 10:51:25 +0330

    add gate_way into project

--- order_app/admin.py
+++ order_app/admin.py
@@ -5,8 +5,8 @@ from .models import (
     OrderItem,
     ShippingCompany,
     ShippingMethod,
-    PaymentGateWay,
-    ResultPaymentGateWay
+    # PaymentGateWay,
+    # ResultPaymentGateWay
 )
 
 
