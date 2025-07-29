428204e1b44fb506220e3b4b54f36f4ea4e12668
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-07-29 10:51:34 +0330

    add celery task send_notification_after_create_ticket

--- account_app/signals.py
+++ account_app/signals.py
@@ -1,10 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
-from .models import User, Profile
+from .models import User, Profile, Ticket
+from .tasks import send_notification_after_create_ticket
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=Ticket)
+def create_notification_ticket(sender, instance, created, **kwargs):
+    if created and not instance.sender.is_staff:
+        send_notification_after_create_ticket.delay(
+            room_id=instance.room.id,
+        )
--- account_app/tasks.py
+++ account_app/tasks.py
@@ -1,4 +1,6 @@
 from celery import shared_task
+
+from account_app.models import User, PrivateNotification
 from core.utils.sms import send_otp_sms
 import asyncio
 
@@ -8,6 +10,24 @@ def send_otp_code_by_celery(phone: str, code: str):
     asyncio.run(send_otp_sms(phone, code))
 
 
-# @shared_task(max_retries=3)
-# def pow_number(x, y):
-#     return x * y
+@shared_task()
+def send_notification_after_create_ticket(room_id):
+    users = User.objects.filter(
+        is_active=True,
+        is_staff=True
+    ).only(
+        "mobile_phone",
+    )
+    notification = [
+        PrivateNotification(
+            user_id=i.id,
+            title="ticket",
+            notif_type="ticket",
+            body="یک تیکت ثبت شده هست",
+            notifi_redirect_url=f'room_id:{room_id}'
+        )
+        for i in users
+    ]
+
+    if notification:
+        PrivateNotification.objects.bulk_create(notification)
