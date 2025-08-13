1a2096395774fc89257450adbfb14f889eb23541
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-08-13 19:46:16 +0330

    better performance  signal update_product_sales

--- order_app/signals.py
+++ order_app/signals.py
@@ -1,14 +1,13 @@
+from django.db.models import F
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from order_app.models import Order
+
 
 @receiver(post_save, sender=Order)
 def update_product_sales(sender, instance, created, **kwargs):
     if instance.is_complete:
         items = instance.order_items.filter(is_active=True)
-
-        if items.exists():
-            for item in items:
-                qy = item.quantity
-                item.product_variant.product.total_sale += qy
-                item.product_variant.product.save()
+        for item in items:
+            item.product_variant.product.total_sale = F('total_sale') + item.quantity
+            item.product_variant.product.save(update_fields=['total_sale'])
