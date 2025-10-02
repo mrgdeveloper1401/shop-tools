db4c5db6e7d6f891553485cad38710d762bdf055
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-10-02 17:10:49 -0400

    add config for pannel admin

--- order_app/admin.py
+++ order_app/admin.py
@@ -12,29 +12,71 @@ from .models import (
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "status",
+        "profile_id",
+        "is_complete",
+        "address_id",
+        "tracking_code",
+        "created_at",
+        "updated_at"
+    )
+    list_filter = ("is_complete",)
+    
 
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "order_id",
+        "product_variant_id",
+        "price",
+        "quantity",
+        "is_active",
+        "created_at",
+        "updated_at"
+    )
+    list_filter = ("is_active",)
 
 
 @admin.register(ShippingCompany)
 class ShippingCompanyAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "name",
+        "is_active",
+        "created_at"
+    )
+    list_filter = ("is_active",)
 
 
 @admin.register(ShippingMethod)
 class ShippingMethodAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "company_id",
+        "name",
+        "shipping_type",
+        "price",
+        "estimated_days",
+        "is_active"
+    )
+    list_filter = ("is_active",)
 
 
 @admin.register(PaymentGateWay)
 class PaymentGateWayAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "order",
+        "id",
+        "created_at"    )
 
 
 @admin.register(VerifyPaymentGateWay)
 class ResultPaymentGateWayAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "payment_gateway_id",
+        "id",
+        "created_at"    
+        )
