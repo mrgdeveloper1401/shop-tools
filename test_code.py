13348a32056982c5793b9c6e6a9dba5b197b14c1
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-10-18 05:07:36 -0400

    edit test_code

--- test_code.py
+++ test_code.py
@@ -1,2 +1,9 @@
-def log_request(request):
-    return request.get_port()
+from rest_framework.request import Request
+from django.http import JsonResponse
+
+def log_request(request: Request):
+    # import ipdb
+    # ipdb.set_trace()
+    return JsonResponse(
+        {"port": request.get_port()},
+    )
