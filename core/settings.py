7dd7f7b71b324460bb9b97e59f395027b5b51211
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-10-18 06:10:10 -0400

    add throttle

--- core/settings.py
+++ core/settings.py
@@ -154,6 +154,14 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '30/minute',  # 30 درخواست در دقیقه برای کاربران ناشناس
+        'user': '60/minute',  # 60 درخواست در دقیقه برای کاربران عادی
+    }
 }
 
 # config JWT settings
