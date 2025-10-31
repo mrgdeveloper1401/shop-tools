91e904e3b5b3fa3bc2e1887ed6fab6d97265ad3f
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-10-31 17:47:35 +0000

    add import queue

--- core/envs/production.py
+++ core/envs/production.py
@@ -2,6 +2,7 @@ from datetime import timedelta, date
 from pathlib import Path
 import os
 from decouple import config, Csv
+from kombu import Queue
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
