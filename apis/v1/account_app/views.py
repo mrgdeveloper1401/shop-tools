99ebd51f941c0bea6d43401955735550c3ff408e
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-12-06 11:56:10 +0330

    .

--- apis/v1/account_app/views.py
+++ apis/v1/account_app/views.py
@@ -34,8 +34,6 @@ class AsyncRequestOtpView(AsyncApiView):
     permission_classes = (AsyncNotAuthenticated,)
 
     async def post(self, request):
-        import ipdb
-        ipdb.set_trace()
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
 
