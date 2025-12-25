4ffc9078788d99fa13c82cb97502ff4964ba0f9a
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-12-25 20:44:24 +0330

    edit performance in django

--- scripts/gunicorn.conf.py
+++ scripts/gunicorn.conf.py
@@ -3,8 +3,8 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "eventlet"
-# worker_connections = 1000
+worker_class = "gthread"
+threads = 5
 
 # Logging
 loglevel = "error"
@@ -13,7 +13,7 @@ accesslog = "-"
 
 # Timeouts
 timeout = 30 # Automatically restart workers if they take too long
-keepalive = 3 # Keep connections alive for 3s
+keepalive = 1 # Keep connections alive for 1s after 1s unlock thread
 graceful_timeout = 30 #  Graceful shutdown for workers
 
 # Worker Restart Settings
@@ -21,7 +21,7 @@ max_requests = 1000  # Restart workers after processing 1000 requests
 max_requests_jitter = 50  # Add randomness to avoid mass restarts
 
 # Process naming
-proc_name = "django_asgi"
+proc_name = "django_shop_tools"
 
 # Gunicorn Configuration for Nginx
-proxy_protocol = True  # Enable proxy support
+# proxy_protocol = True  # Enable proxy support
