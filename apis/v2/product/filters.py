0ad583591dfec69b3285d2b16fac50716d4171a9
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-02-11 15:42:58 +0330

    add filter has_discount

--- apis/v2/product/filters.py
+++ apis/v2/product/filters.py
@@ -1,11 +1,28 @@
-from django_filters.rest_framework import FilterSet
+from django.db.models import OuterRef, Exists
+from django_filters.rest_framework import FilterSet, BooleanFilter
 
+from discount_app.models import ProductDiscount
 from product_app.models import ProductVariant
 
 
 class ProductVariantFilter(FilterSet):
+    has_discount = BooleanFilter(method='filter_has_discount')
+
     class Meta:
         model = ProductVariant
         fields = {
             "name": ("icontains",),
         }
+
+    def filter_has_discount(self, queryset, name, value):
+        if value is None:
+            return queryset
+        else:
+            active_discount = ProductDiscount.objects.filter(
+                product_variant=OuterRef("pk"),
+            ).valid_discount()
+
+            if value is True:
+                return queryset.filter(Exists(active_discount))
+            else:
+                return queryset.filter(~Exists(active_discount))
