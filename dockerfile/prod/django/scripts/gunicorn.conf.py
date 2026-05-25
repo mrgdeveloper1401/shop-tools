cf7bf2fcfaacfbefe10a69b03302e4336224a821
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-05-25 15:28:53 +0330

    update gunicorn.conf.py and nginx.conf

--- dockerfile/prod/django/scripts/gunicorn.conf.py
+++ dockerfile/prod/django/scripts/gunicorn.conf.py
@@ -4,24 +4,24 @@ import multiprocessing
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gthread"
-threads = 5
+threads = 4
 
 # Logging
-loglevel = "error"
+loglevel = "warning"
 errorlog = "-"
-accesslog = "-"
 
-# Timeout
-# timeout = 60 # Automatically restart workers if they take too long
-keepalive = 1 # Keep connections alive for 1s after 1s unlock thread
-# graceful_timeout = 30 #  Graceful shutdown for workers
+timeout = 20
+keepalive = 5 # Keep connections alive for 1s after 1s unlock thread
+graceful_timeout = 30 #  Graceful shutdown for workers
 
 # Worker Restart Settings
-# max_requests = 1000  # Restart workers after processing 1000 requests
-# max_requests_jitter = 50  # Add randomness to avoid mass restarts
+max_requests = 1000  # Restart workers after processing 1000 requests
+max_requests_jitter = 100  # Add randomness to avoid mass restarts
 
 # Process naming
 proc_name = "django_shop_tools"
 
-# Gunicorn Configuration for Nginx
-# proxy_protocol = True  # Enable proxy support
+preload_app = True # Fast start of workers
+
+# server machine
+reuse_port = True
