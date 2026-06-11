1c2689837dad8e1f7783c49da94d6ed6cc5e2497
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-06-11 23:32:15 +0330

    update nginx.conf and gunicorn.conf.py

--- dockerfile/prod/django/scripts/gunicorn.conf.py
+++ dockerfile/prod/django/scripts/gunicorn.conf.py
@@ -7,16 +7,16 @@ worker_class = "gthread"
 threads = 4
 
 # Logging
-loglevel = "warning"
+loglevel = "error"
 errorlog = "-"
 
-timeout = 20
+timeout = 30
 keepalive = 5 # Keep connections alive for 1s after 1s unlock thread
 graceful_timeout = 30 #  Graceful shutdown for workers
 
 # Worker Restart Settings
 max_requests = 1000  # Restart workers after processing 1000 requests
-max_requests_jitter = 100  # Add randomness to avoid mass restarts
+max_requests_jitter = 150  # Add randomness to avoid mass restarts
 
 # Process naming
 proc_name = "django_shop_tools"
@@ -25,3 +25,9 @@ preload_app = True # Fast start of workers
 
 # server machine
 reuse_port = True
+
+def post_fork(server, worker):
+    from django.db.backends.postgresql.base import DatabaseWrapper
+
+    # clear connection pool where preload_app equal true and inheritance worker
+    DatabaseWrapper._connection_pools.clear()
